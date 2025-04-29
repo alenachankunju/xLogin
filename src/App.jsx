f62bc03a02fdb23be42086821d42bfc57ba1d4f8
alenachankunju <alenachankunju@gmail.com>
2025-04-29 20:34:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import XLogin from "./components/XLogin";
+
+function App() {
+  
+
+  return (
+    <>
+      <XLogin />
+    </>
+  );
+}
+
+export default App;
